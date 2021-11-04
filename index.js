84e428e9c758d6d70d1817563996127153e7c12d
ZhengDS <dianshuangzheng@163.com>
2021-11-04 11:37:20 +0800

    feat: render

--- index.js
+++ index.js
@@ -1,16 +1,41 @@
 const element = {
-    type: 'h1',
+    type: "div",
     props: {
-        title: "foo",
-        children: "Hello",
-    },
-}
-const container = document.getElementById('root');
-// What hanppend when we use jsx
-const node = document.createElement(element.type)
-node["title"] = element.props.title
-const text = document.createTextNode("")
-text["nodeValue"] = element.props.children
-node.appendChild(text)
-container.appendChild(node)
-
+      id: "container",
+      children: [
+        { type: "input", props: { value: "foo", type: "text" } },
+        { type: "a", props: { href: "/bar" } },
+        { type: "span", props: {} }
+      ]
+    }
+  };
+  const textElement = {
+    type: "span",
+    props: {
+      children: [
+        {
+          type: "TEXT ELEMENT", // 1
+          props: { nodeValue: "Foo" } // 2
+        }
+      ]
+    }
+  };
+  
+function render(element, parentDom) {
+    const {type, props} = element;
+    const isTextElement = type === 'TEXT ELEMENt';
+    const dom = isTextElement 
+    ? document.createTextNode('')
+    : document.createElement(type);
+    const isListener = (name='') => name.startsWith(on);
+    Object.keys(props).filter(isListener).forEach(name => {
+        const eventType = name.toLowerCase().substring(2);
+        dom.addEventListener(eventType, props[name]);
+    })
+    const isAttribute = (name = '') => !isListener(name) && name !== 'children';
+    Object.keys(props).filter(isAttribute).forEach(name => dam[name] = props[name]);
+    const childElements = props.children || [];
+    childElements.forEach(childElements => render(childElements, dom));
+    parentDom.appendChild(element);
+  }
+  
